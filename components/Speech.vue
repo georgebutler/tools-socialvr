bf1e2743db0c1503ca79b568770f4997390a113b
George Butler <georgebutleriv@gmail.com>
2021-08-09 13:18:14 -0700

    Finished

--- components/Speech.vue
+++ components/Speech.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    Speech Event
+  <div class="">
+    <input type="checkbox">
+    <span class="p-1"><strong>{{ speaker }}</strong> spoke for {{ duration }} seconds.</span>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    speaker: {
+      type: String,
+      default: 'Unknown'
+    },
+    duration: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,41 @@
 <template>
-  <div class="container">
-    <div class="section">
-      <div class="field">
-        <textarea v-model="input" spellcheck="false" class="textarea" :class="{ 'is-danger': error.length > 0}" placeholder="Paste study data here" />
-        <p v-show="error.length > 0" class="help is-danger">
-          {{ error }}
+  <main>
+    <div class="container">
+      <div class="section">
+        <h1 class="title">
+          Social VR Study Data Tool
+        </h1>
+        <div class="field">
+          <textarea v-model="input" spellcheck="false" class="textarea" :class="{ 'is-danger': error.length > 0}" placeholder="Paste study data here" />
+          <p v-show="error.length > 0" class="help is-danger">
+            {{ error }}
+          </p>
+        </div>
+        <div class="field">
+          <button class="button is-primary is-fullwidth" @click="convert">
+            Convert
+          </button>
+        </div>
+      </div>
+      <div class="section">
+        <h2 class="subtitle">
+          Speech Events
+        </h2>
+        <p v-if="speechEvents.length <= 0">
+          Speech events will be listed here
         </p>
-      </div>
-      <div class="field">
-        <button class="button is-primary is-fullwidth" @click="convert">
-          Convert
-        </button>
+        <Speech v-for="speechEvent in speechEvents" :key="speechEvent.key" :speaker="speechEvent.speaker" :duration="speechEvent.duration" />
       </div>
     </div>
-    <div class="section">
-      <Speech v-for="speechEvent in speechEvents" :key="speechEvent.key" />
-    </div>
-  </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Social VR Study Data Tool</strong> by <a href="https://github.com/georgebutler">George Butler</a>.
+        </p>
+        <p>The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.</p>
+      </div>
+    </footer>
+  </main>
 </template>
 
 <script>
@@ -39,11 +58,9 @@ export default {
 
         result.forEach((event, index) => {
           if (index < result.length - 1) {
-            const seconds = (result[index + 1].timestamp - event.timestamp) / 1000
-
             this.speechEvents.push({
-              speakerName: event.speakerName,
-              speechDuration: seconds,
+              speaker: event.speakerName || 'Null',
+              duration: Math.max((result[index + 1].timestamp - event.timestamp) / 1000, 0),
               key: Math.floor(Math.random() * Date.now())
             })
           }
